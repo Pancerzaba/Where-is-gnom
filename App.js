441e5b9e95c63c9158d74462c494ae56b5c325aa
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-12-08 13:01:14 +0100

    add database

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+//import {applyMiddleware} from 'redux';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import GnomScreen from './screen/GnomScreen'
 import GnomsNavigator from './navigation/GnomsNavigation';
+import User from './database/User'
+
+import database from '@react-native-firebase/database';
+
+const reference = database().ref('/users/123');
 
 
 export default function App() {
-
+ 
   return (
-    <GnomsNavigator/>
-    // <View style={styles.container}>
-    //   {/* <StartScreen/> */}
-    //   {/* <SearchScreen/> */}
-      // <GnomScreen/>
-    //   {/* <GalleryScreen/> */}
-    // </View>
+    <User/>
+   // <GnomsNavigator/>
+    
   );
 }
 
