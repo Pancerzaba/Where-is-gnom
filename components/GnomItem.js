224d5d1caae8f2cbb271de141e9b4451d5dcab1c
Marta Rozmysłowicz <marta.rozmyslowicz21@gmail.com>
2021-11-23 21:25:00 +0100

    update

--- components/GnomItem.js
+++ components/GnomItem.js
@@ -1,68 +1,62 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image,Button } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
 
+const ProductItem = (props) => {
+  return (
+    <View style={styles.product}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: props.image }} />
+      </View>
+      <View style={styles.details}>
+        <Text style={styles.title}>{props.title}</Text>
+      </View>
+    </View>
+  );
+};
 
-const ProductItem = props => {
-    return (
-        <View style={styles.product}>
-            <View style={styles.imageContainer}>
-                <Image style={styles.image} source={{uri: props.image}}/>
-
-            </View>
-            <View style={styles.details}>
-                <Text style={styles.title}>{props.title}</Text>
-               
-            </View>
-          
-     
-        </View>
-    )
-}
-
-export default ProductItem
+export default ProductItem;
 
 const styles = StyleSheet.create({
-    product: {
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 8,
-        elevation: 8,
-        borderRadius: 10,
-        backgroundColor: 'white',
-        height: 250,
-        margin: 20
-    },
-    imageContainer:{
-        width: '100%',
-        height: '80%',
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
-        overflow: 'hidden'
-    },
-    image:{
-        width: '100%',
-        height: '100%'
-    },
-    details: {
-        alignItems: 'center',
-        height: '15%',
-        padding: 10
-    },
-    title:{
-        fontSize: 18,
-        marginVertical: 4,
-    },
-    price:{
-        fontSize: 14,
-        color: '#888'
-    },
-    actions: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '25%',
-        paddingHorizontal:20
-    }
-
-})
+  product: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 8,
+    borderRadius: 10,
+    backgroundColor: "white",
+    height: 300,
+    margin: 20,
+  },
+  imageContainer: {
+    width: "100%",
+    height: "80%",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  details: {
+    alignItems: "center",
+    height: "20%",
+    padding: 10,
+  },
+  title: {
+    fontSize: 18,
+    marginVertical: 4,
+  },
+  price: {
+    fontSize: 14,
+    color: "#888",
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "25%",
+    paddingHorizontal: 20,
+  },
+});
--- screen/GalleryScreen.js
+++ screen/GalleryScreen.js
@@ -1,108 +1,111 @@
-import React, { useEffect } from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList, ScrollView } from 'react-native'
-import MainButton from '../components/MainButtons'
-import {fetchGnomes} from '../store/actions/Gnom'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  ScrollView,
+} from "react-native";
+import MainButton from "../components/MainButtons";
+import { fetchGnomes } from "../store/actions/Gnom";
 
+import GnomItem from "../components/GnomItem";
 
+const GalleryScreen = (props) => {
+  const { availableGnoms } = useSelector((state) => state.gnoms);
+  const dispatch = useDispatch();
 
-import GnomItem from '../components/GnomItem'
-
-const GalleryScreen = props => {
-
-    const {availableGnoms} = useSelector(state => state.gnoms)
-    const dispatch = useDispatch()
-
-
-    useEffect(() => {
-       dispatch(fetchGnomes())
-    }, [])
-    let ButtonComponent =TouchableOpacity;
-    return ( 
-      <ScrollView>
-        <View style={styles.container}>
-            {/* <View style={styles.buttons}>
+  useEffect(() => {
+    dispatch(fetchGnomes());
+  }, []);
+  let ButtonComponent = TouchableOpacity;
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        {/* <View style={styles.buttons}>
                 <MainButton>Wszystkie</MainButton>
                 <MainButton>Zebrane</MainButton>
                 <MainButton>Nie Zebrane</MainButton>
             </View> */}
-            <View style={styles.fotos}>
-                <FlatList data={availableGnoms} 
-                    keyExtractor={item=>item.id}
-                    renderItem={
-                        itemData=>
-                        <ButtonComponent  onPress={()=>{
-                            props.navigation.navigate('Gnom',{
-                                gnomId: itemData.item.id,
-                                productTitle: itemData.item.title
-                            })
-                            }}>
-                            <GnomItem image={itemData.item.imageURL} 
-                            title={itemData.item.title}  />
-                        </ButtonComponent> }
+        <View style={styles.fotos}>
+          <FlatList
+            data={availableGnoms}
+            keyExtractor={(item) => item.id}
+            renderItem={(itemData) => (
+              <ButtonComponent
+                onPress={() => {
+                  props.navigation.navigate("Gnom", {
+                    gnomId: itemData.item.id,
+                    productTitle: itemData.item.title,
+                  });
+                }}
+              >
+                <GnomItem
+                  image={itemData.item.imageURL}
+                  title={itemData.item.title}
                 />
-                </View>
-            </View>
-            </ScrollView>
-    )
-}
-GalleryScreen.navigationOptions={
-    headerTitle: 'Gallery ',
-    headerStyle: {
-        backgroundColor: '#452187',
-    },
-    headerTintColor: 'white'
-}
-export default GalleryScreen
+              </ButtonComponent>
+            )}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+GalleryScreen.navigationOptions = {
+  headerTitle: "Gallery ",
+  headerStyle: {
+    backgroundColor: "#452187",
+  },
+  headerTintColor: "white",
+};
+export default GalleryScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        display: "flex",
-        flexDirection: 'column',
-        //height: '100%',
-        //width: '100%',
-        justifyContent: 'space-evenly',
-        flexWrap: 'wrap',
-//backgroundColor:'green',
-alignItems: 'center'
-        // alignItems: 'center'
-
-    },
-    title:{
-        fontSize: 20,
-        //fontWeight: 'bold',
-        textAlign: 'center',
-        backgroundColor:'#452187',
-        color: 'white',
-        padding: '10px',
-        //borderRadius: 25,
-        margin: 10
-    },
-    buttons:{
-       //display: "flex",
-        // alignItems: 'center',
-       // backgroundColor: 'red',
-        alignItems: 'center',
-        flexDirection: 'row',
-        width: 320,
-        height: 100,
-        margin: 10
-      
-        
-       
-    },
-    foto:{
-        
-        width: 150,
-        margin: 10,
-        flex: 1,
-        display: "flex"
-    },
-    fotos:{
-        display: "flex",
-        flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        
-    }
-})
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    //height: '100%',
+    //width: '100%',
+    justifyContent: "space-evenly",
+    flexWrap: "wrap",
+    //backgroundColor:'green',
+    alignItems: "center",
+    // alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    //fontWeight: 'bold',
+    textAlign: "center",
+    backgroundColor: "#452187",
+    color: "white",
+    padding: "10px",
+    //borderRadius: 25,
+    margin: 10,
+  },
+  buttons: {
+    //display: "flex",
+    // alignItems: 'center',
+    // backgroundColor: 'red',
+    alignItems: "center",
+    flexDirection: "row",
+    width: 320,
+    height: 100,
+    margin: 10,
+  },
+  foto: {
+    width: 150,
+    margin: 10,
+    flex: 1,
+    display: "flex",
+  },
+  fotos: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+});
