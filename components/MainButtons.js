515a46a17b42a44963ddfe7fa929c6f095edb020
Marta Rozmysłowicz <marta.rozmyslowicz21@gmail.com>
2021-11-30 15:00:35 +0100

    zmiana wyglądu strony głownej

--- components/MainButtons.js
+++ components/MainButtons.js
@@ -1,53 +1,53 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
 
+const MainButton = (props) => {
+  let ButtonComponent = TouchableOpacity;
 
-const MainButton = props => {
-  let ButtonComponent =TouchableOpacity;
-
-  if(Platform.OS === 'android' && Platform.Version>=21){
+  if (Platform.OS === "android" && Platform.Version >= 21) {
     ButtonComponent = TouchableNativeFeedback;
   }
 
   return (
     <View style={styles.buttonContainer}>
-    <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>{props.children}</Text>
-      </View>
-    </ButtonComponent></View>
+      <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </ButtonComponent>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: '#452187',
-    //height:' 50%',
-    //width: '100%',
-    margin: 2,
-  alignItems: 'center'
- 
+    backgroundColor: "rgba(80,160,255,0.9)",
+    margin: 3,
+    alignItems: "center",
   },
-  buttonContainer:{
+  buttonContainer: {
     borderRadius: 5,
-    //overflow: 'hidden',
-   // margin:5,
+    // overflow: "hidden",
     padding: 5,
-    //backgroundColor: 'silver',
-  alignItems: 'center'
-   
+    alignItems: "center",
   },
   buttonText: {
-    display: 'flex',
+    display: "flex",
     margin: 5,
-    color: 'white',
- //backgroundColor:'red',
-    fontSize: 18,
-    textAlign: 'center',
-   // marginVertical: 10,
-    padding: 2,
-    //backgroundColor: 'red'
-  }
+    color: "white",
+
+    fontSize: 20,
+    textAlign: "center",
+    // marginVertical: 10,
+    padding: 5,
+  },
 });
 
 export default MainButton;
--- screen/StartScreen.js
+++ screen/StartScreen.js
@@ -1,58 +1,70 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image, Button } from 'react-native'
-import MyButton from '../components/MainButtons'
+import React from "react";
+import {
+  StyleSheet,
+  ImageBackground,
+  Text,
+  View,
+  Image,
+  Button,
+} from "react-native";
+import MyButton from "../components/MainButtons";
 
-const StartScreen = props => {
-    return (
-
-        <View style={styles.container}>
-            <View style={styles.logoContainer}>
-                <Image style={styles.Logo} source={require('../assets/logov1.png')} />
-            </View>
-            <View style={styles.buttons}>
-                <MyButton  onPress={()=>{
-                    props.navigation.navigate({routeName: 'Search'})
-                 
-                }} >Znajdz krasnala</MyButton>
-                <MyButton  onPress={()=>{
-                    props.navigation.navigate({routeName:'Gallery'})
-                }} >Galeria krasnali</MyButton>
-                {/* <MyButton >Wydarzenia</MyButton> */}
-
-
-            </View>
+const StartScreen = (props) => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.container}
+        source={require("../assets/papaKrasnal.jpg")}
+      >
+        <View style={styles.overlay}>
+          <View style={styles.buttons}>
+            <MyButton
+              onPress={() => {
+                props.navigation.navigate("Search");
+              }}
+            >
+              Znajdz krasnala
+            </MyButton>
+            <MyButton
+              onPress={() => {
+                props.navigation.navigate({ routeName: "Gallery" });
+              }}
+            >
+              Galeria krasnali
+            </MyButton>
+            {/* <MyButton >Wydarzenia</MyButton> */}
+          </View>
         </View>
-    )
-}
-StartScreen.navigationOptions={
-    headerTitle: ' ',
-    headerStyle: {
-        backgroundColor: '#452187',
-    },
-    headerTintColor: 'white'
-}
-export default StartScreen
+      </ImageBackground>
+    </View>
+  );
+};
+StartScreen.navigationOptions = {
+  headerTitle: " ",
+  headerStyle: {
+    backgroundColor: "#452187",
+  },
+  headerTintColor: "white",
+};
+export default StartScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center'
-    },
-    buttons: {
-        height: 150,
-        //alignItems: 'center',
-        justifyContent: 'space-between',
-        width: '90%'
+  container: {
+    flex: 1,
+  },
+  overlay: {
+    flex: 1,
+    height: 100,
+    backgroundColor: "rgba(10,10,10,0.6)",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
 
-    }, Logo: {
-        width: 150,
-        height: 150,
-    
-    },
-    logoContainer:{
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        padding: 15,
-        height: 300
-    }
-})
+  buttons: {
+    height: "40%",
+    // flexDirection: "column",
+    // justifyContent: "space-between",
+
+    // backgroundColor: "rgba(10,10,10,0.6)",
+  },
+});
