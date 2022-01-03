127e1cb754351d718c1e3d7be0b2081c60f6aec4
Marta Rozmysłowicz <marta.rozmyslowicz21@gmail.com>
2022-01-03 14:18:53 +0100

    dodawanie nowego krasnala do bazy

--- screen/AddGnomeScreen.js
+++ screen/AddGnomeScreen.js
@@ -1,37 +1,85 @@
 import React from "react";
-import { StyleSheet, Text, Image, TextInput, View } from "react-native";
+import { StyleSheet, Text, Image, TextInput, View, Alert } from "react-native";
 import MainButton from "../components/MainButtons";
+import * as Location from "expo-location";
+import { useFirestore } from "../hooks/useFirestore";
 
 const AddGnome = () => {
-  const [mail, setMail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [displayName, setDisplayName] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [adress, setAdress] = React.useState("");
+  const [lat, setLat] = React.useState(null);
+  const [lng, setLng] = React.useState(null);
+  const [imageURL, setImageURL] = React.useState();
+  const [location, setLocation] = React.useState(null);
+
+  const { addDocument, response } = useFirestore("gnomes");
+
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  };
+
+  React.useEffect(() => {
+    getLocation();
+  }, []);
+
+  React.useEffect(() => {
+    location &&
+      (setLat(location.coords.latitude), setLng(location.coords.longitude));
+  }, [location]);
+
+  const handleAddDwarf = async () => {
+    const dwarf = {
+      title,
+      adress,
+      description,
+      lat,
+      lng,
+    };
+
+    await addDocument(dwarf);
+
+    setTitle("");
+    setAdress("");
+    setDescription("");
+
+    if (!response.error) {
+      Alert.alert("Dodano nowego krasnala");
+    }
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.form}>
         {/* <Text>Dodaj gnoma</Text> */}
         <MainButton onPress={() => {}}>Dodaj zdjęcie</MainButton>
         <TextInput
           style={styles.input}
-          onChangeText={(text) => setMail(text)}
-          value={mail}
+          onChangeText={(text) => setTitle(text)}
+          value={title}
           placeholder="Nazwa"
         />
 
         <TextInput
           style={styles.input}
-          onChangeText={(text) => setDisplayName(text)}
-          value={displayName}
+          onChangeText={(text) => setAdress(text)}
+          value={adress}
           placeholder="Gdzie stoi"
         />
         <TextInput
           style={styles.inputLong}
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry
+          onChangeText={(text) => setDescription(text)}
+          value={description}
           placeholder="Opis"
         />
-        <MainButton onPress={() => {}}>Dodaj krasnala</MainButton>
+        <MainButton onPress={handleAddDwarf}>Dodaj krasnala</MainButton>
       </View>
     </View>
   );
@@ -40,15 +88,17 @@ const AddGnome = () => {
 export default AddGnome;
 
 const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: "red",
+    width: "100%",
+    height: "100%",
+  },
   form: {
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "column",
-    // width: "80%",
-    // height: "80%",
     paddingHorizontal: 10,
     paddingVertical: 20,
-    // backgroundColor: "grey",
   },
   input: {
     width: "100%",
